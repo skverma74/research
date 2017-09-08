28496f1e1be97bf46c11d0d9e3b7dd4509c03062
Alka Sachan <alka_v75@hotmail.com>
2017-09-08 16:46:10 -0700

    updated

--- WebApp1/Program.cs
+++ WebApp1/Program.cs
@@ -11,6 +11,7 @@ namespace WebApp1
     {
         public static void Main(string[] args)
         {
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
